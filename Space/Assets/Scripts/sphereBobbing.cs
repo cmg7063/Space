c738a276c699ada445ee6e08ea53fcf3dff916e0
JamesFriedenberg <jpf9146@g.rit.edu>
2017-11-21 04:30:22 -0500

    Added Level 2 Graybox / Scripted bobbing objects
    Capsules are guaranteed enemy spawns.

---
+++ Space/Assets/Scripts/sphereBobbing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereBobbing : MonoBehaviour
+{
+    private Vector3 bobbingVec;
+    private float originalY;
+
+    public float bobForce;
+	// Use this for initialization
+	void Start ()
+    {
+        this.originalY = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bobbingVec = transform.position;
+        bobbingVec.y = originalY + (Mathf.Sin(Time.time) * bobForce);
+        transform.position = bobbingVec;
+    }
+}
