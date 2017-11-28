bc487b433ec5087baf6cb09a93806440becde515
Corinne Green <cmg7063@g.rit.edu>
2017-11-27 23:27:09 -0500

    title and menu screen
    fixed auto generated lighting in each scene, added each scene to build

---
+++ Space/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+
+    public Button instructions;
+    public Button play;
+    public Button quit;
+    public Button options;
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Title"))
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
---
+++ Space/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,53 @@
+﻿///SmoothFollow Script. 
+/// This script was not written by me. It was provided by the professor.
+/// This script is attached to the Camera, allows for a smooth follow through the scene.
+
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour 
+{
+	public Transform target;
+	public float distance;
+	public float height;
+	public float heightDamping;
+	public float positionDamping;
+	public float rotationDamping;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		// Early out if we don't have a target
+		if (!target)
+			return;
+		
+		float wantedHeight = target.position.y + height;
+		float currentHeight = transform.position.y;
+		
+		// Damp the height
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+		
+		// Set the position of the camera 
+		Vector3 wantedPosition = target.position - target.forward * distance;
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * positionDamping);
+		
+		// adjust the height of the camera
+		transform.position = new Vector3 (transform.position.x, currentHeight, transform.position.z);
+		
+		
+		//transform.forward = Vector3.Lerp (transform.forward, target.position - transform.position, Time.deltaTime * rotationDamping);
+		transform.forward = Vector3.Lerp (transform.forward, target.forward , Time.deltaTime * rotationDamping);
+		
+	}
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
