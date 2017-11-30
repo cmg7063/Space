23611f331d17a79d9e60e442612662a7fdc38c65
Zanthous <bcm023@gmail.com>
2017-11-30 09:01:56 -0500

    Humanoid AI pretty much complete minus shooting states
    patrolling and finding cover work properly now, fixed gitignore stuff earlier too

--- Space/Assets/Scripts/HumanoidAI.cs
+++ Space/Assets/Scripts/HumanoidAI.cs
@@ -7,10 +7,16 @@ public class HumanoidAI : MonoBehaviour
 {
     private List<Vector3> patrolWaypoints = new List<Vector3>();
     private List<Vector3> coverLocations = new List<Vector3>();
+
+    private List<Vector3> strafeLocations = new List<Vector3>();
+    private bool positionsInvalid = false;
+
     private int targetIndex = -1;//cover
     private int patrolIndex = -1;
+    private int strafeIndex = -1;
 
-	private List<Ray> DebugRays = new List<Ray> ();
+    private List<Vector3> DebugRaysOrigin = new List<Vector3> ();
+    private List<Vector3> DebugRaysDirection = new List<Vector3> ();
 
 
     public GameObject player;
@@ -18,8 +24,8 @@ public class HumanoidAI : MonoBehaviour
     private float shootRange = 20.0f;
     private float alertRange = 35.0f;
 
-    private float aiHeight = 2.0f;
-    private float playerHeight = 2.0f;
+    private float aiHeight = 1.9f;
+    private float playerHeight = 1.9f;
 
     private float speed = 3.5f;
 
@@ -81,7 +87,7 @@ public class HumanoidAI : MonoBehaviour
         //state logic
         if( state == HumanoidAIState.GoingToCover)
         {
-            speed = 7.0f;   
+            speed = 9.0f;   
 
             if(coverLocations.Count == 0)
             {
@@ -111,7 +117,7 @@ public class HumanoidAI : MonoBehaviour
                 }
             }
             //If on the way to cover the AI is in range and sees the player it switches to shooting
-            if( distanceToPlayer < shootRange && CheckLineOfSightPlayer() )
+            if( distanceToPlayer < shootRange && IsPlayerInLineOfSight() )
             {
                 targetIndex = -1;
                 state = HumanoidAIState.Shooting;
@@ -126,14 +132,16 @@ public class HumanoidAI : MonoBehaviour
             {
                 state = HumanoidAIState.ShootingFromCover;
             }
+            if( distanceToPlayer > shootRange && IsPlayerInLineOfSight() )
+            {
+                state = HumanoidAIState.GoingToCover;
+            }
         }
 
         if( state == HumanoidAIState.Patrolling)
         {
 
             agent.autoBraking = false;
-
-            
 
             if( !agent.pathPending && agent.remainingDistance < 0.5f )
                 GotoNextPoint();
@@ -152,15 +160,77 @@ public class HumanoidAI : MonoBehaviour
         }
         if( state == HumanoidAIState.Shooting)
         {
+
+            agent.autoBraking = false;
             agent.SetDestination( transform.position );
+            if( distanceToPlayer > shootRange )
+            {
+                //always reset shooting upon leaving (changing states)
+                ResetShooting();
+                state = HumanoidAIState.GoingToCover;
+            }
+            /* not working properly yet
+            //only do this once
+            if( strafeLocations.Count == 0 && !positionsInvalid )
+            { 
+                //generate two points to strafe between that are valid paths and also spots that have vision of the player
+                Vector3 toPlayer = VectorToPlayer();
+
+                //TODO test and make sure this is correct
+                Vector3 perpToPlayer = Vector3.Cross(toPlayer,Vector3.up).normalized;
+
+                List<Vector3> potentialPoints = new List<Vector3>();
+
+                potentialPoints.Add(transform.position + perpToPlayer);
+                potentialPoints.Add( transform.position - perpToPlayer );
+
+                NavMeshHit nmHit;
+
+                //Sample position finds a valid location close to a point 
+
+                if( NavMesh.SamplePosition( potentialPoints[ 0 ], out nmHit, 1, NavMesh.AllAreas ) )
+                {
+                    strafeLocations.Add( nmHit.position );
+                }
+                else if( NavMesh.SamplePosition( potentialPoints[ 0 ], out nmHit, 1, NavMesh.AllAreas ) ) 
+                {
+                    strafeLocations.Add( nmHit.position );
+                }
+                else
+                {
+                    //stop moving [temporary]
+                    agent.SetDestination( transform.position );
+                    positionsInvalid = true;
+                }
+                //if no points are valid, just stand still
+            }
+            else
+            {
+                //TODO make sure .1f isnt too small
+                if( !agent.pathPending && agent.remainingDistance < 0.4f )
+                    StrafeToNextPoint();
+
+                //shoot if can see the player here
+
+            }
+            */
         }
-		for( int index = 0; index < DebugRays.Count; index++)
+		for( int index = 0; index < DebugRaysOrigin.Count; index++)
 		{
-			Debug.DrawRay(DebugRays[index].origin, DebugRays[index].direction);
+            Debug.DrawRay( DebugRaysOrigin[ index ], DebugRaysDirection[ index ], Color.blue, 2.0f );
 		}
+
         agent.speed = speed;
         previousState = state;
     }
+    void ResetShooting()
+    {
+        agent.autoBraking = true;   
+        positionsInvalid = false;
+        strafeLocations.Clear();
+    }
+
+    //combine and generalize these functions after
     void GotoNextPoint()
     {
         if( patrolWaypoints.Count == 0 )
@@ -176,7 +246,29 @@ public class HumanoidAI : MonoBehaviour
         patrolIndex = ( patrolIndex + 1 ) % patrolWaypoints.Count;
     }
 
-    bool CheckLineOfSightPlayer()
+    void StrafeToNextPoint()
+    {
+        if( strafeLocations.Count == 0 )
+            return;
+
+        if( strafeIndex == -1 )
+        {
+            strafeIndex = 0;
+        }
+
+        agent.destination = strafeLocations[ strafeIndex ];
+
+        strafeIndex = ( strafeIndex + 1 ) % strafeLocations.Count;
+    }
+
+    //Y value might have to be included in the future?
+    Vector3 VectorToPlayer()
+    {
+        Vector3 temp = player.transform.position - transform.position;
+        return new Vector3( temp.x, 0.0f, temp.z );
+    }
+
+    bool IsPlayerInLineOfSight()
     {
         //eye level
         Vector3 eye = new Vector3(transform.position.x,
@@ -189,8 +281,9 @@ public class HumanoidAI : MonoBehaviour
         Vector3 direction = playerEye - eye;
         RaycastHit hit;
         Physics.Raycast( eye, direction, out hit);
-
-		if (Physics.Raycast (eye, direction, out hit)) 
+        DebugRaysOrigin.Add( eye );
+        DebugRaysDirection.Add( direction );
+        if (Physics.Raycast (eye, direction, out hit)) 
 		{
 			return (hit.transform.tag == "Player");
 		} 
@@ -236,8 +329,9 @@ public class HumanoidAI : MonoBehaviour
                 Vector3 direction = playerEye - eye;
                 
                 RaycastHit hit;
-				DebugRays.Add (new Ray (playerEye, direction));
-				if (Physics.Raycast (playerEye, direction, out hit)) 
+                DebugRaysOrigin.Add( eye );
+                DebugRaysDirection.Add( direction );
+                if (Physics.Raycast (eye, direction, out hit)) 
 				{
 					if ((hit.transform.tag != "Player")) {
 						index = i;
