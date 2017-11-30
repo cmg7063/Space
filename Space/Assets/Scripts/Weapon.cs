5a6d96c29e6a384529c6eb02fa1e00297402a1f7
axf4764 <axf4764@rit.edu>
2017-11-30 02:28:05 -0500

    Added basic shooting and damage

---
+++ Space/Assets/Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    //Weapon Damage
+    public float damage = 10f;
+    //Weapon Range
+    public float range = 100f;
+    //Fire Rate
+    public float fireRate = 1f;
+    //Magazine Size
+    public uint magazineSize = 30;
+    //Total Ammo Count
+    public uint totalAmmo = 90;
+    //Max ammo count
+    public const uint maxAmmo = 180;
+    //Is the weapon automatic
+    public bool automaticFireRate = true;  
+    //Main Camera
+    public Camera fpscam;
+    //Muzzle Flash
+    public ParticleSystem muzzleFlash;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (automaticFireRate)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Shoot();
+            }
+        } else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Space/Assets/Textures/muzzleflares/Readme.txt
@@ -0,0 +1,13 @@
+You are free to use this muzzle flare pack in your projects.
+
+If you would like to credit me then please credit like this:
+
+Muzzle Flares From
+PixelVFX
+tinyurl.com/PixelVFX
+
+Use this for whatever you want. its public domain. Credit would be nice though.
+
+
+Thank you for using this pack. If you have any suggestions then please PM me on Reddit /u/H4MSTER
+
