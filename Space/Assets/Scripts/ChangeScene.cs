85f85b5b865e5bde0659064cb1d8674cdf8863c0
Duncan Lord <dxl1435@g.rit.edu>
2017-12-07 10:45:18 -0500

    Linked all scenes

---
+++ Space/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour 
+{
+	public string targetSceneName;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void OnTriggerEnter( Collider collision )
+	{
+		if( collision.gameObject.tag == "Player" )
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadSceneAsync( targetSceneName );
+		}
+	}
+}
